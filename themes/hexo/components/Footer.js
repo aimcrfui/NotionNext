0e438eed3766adb45b9038e214001d616172d3b7
aimcrfui <43334837+aimcrfui@users.noreply.github.com>
2022-09-23 14:17:16 +0800

    up

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -12,13 +12,17 @@ const Footer = ({ title }) => {
     >
       <DarkModeButton/>
 
+      <i className='fas fa-copyright' /> {`${startYear}${currentYear}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a><br/>
+
+      {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br/></>}
+
       <span className='hidden busuanzi_container_site_pv'>
             <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
       <span className='pl-2 hidden busuanzi_container_site_uv'>
         <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
         <br/>
-        <h1></h1>
-        <span className='text-xs font-serif'>Powered by <a href='https://www.uuuoo.tk' className='underline dark:text-gray-300'>NotionNext</a></span></span><br/>
+        <h1>{title}</h1>
+        <span className='text-xs font-serif'>Powered by <a href='https://www.uuuoo.tk' className='underline dark:text-gray-300'>NotionNext {BLOG.VERSION}</a></span></span><br/>
 
     </footer>
   )
